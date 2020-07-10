0073988a0f451f8e10dffc7677ffe2af23c71cbd
Vladimir V <67753403+Vovije@users.noreply.github.com>
2020-07-10 12:15:54 +0300

    VovaSergio v.001
    Версия с движением и анимацией перса

---
+++ VovaSergio/Assets/Scripts/DieSpace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class DieSpace : MonoBehaviour
+{
+    public GameObject Respawn;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = Respawn.transform.position;
+        }
+    }
+}
